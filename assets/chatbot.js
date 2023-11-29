bc642d9c3495bd6777649e045617174c8d5be1d5
Parth Madaan <122528926+parthdacoder@users.noreply.github.com>
2023-11-30 02:49:21 +0530

    Update chatbot.js

--- assets/chatbot.js
+++ assets/chatbot.js
@@ -1,11 +1,16 @@
 window.watsonAssistantChatOptions = {
-  integrationID: "df4520b1-acfe-4866-af67-8500819548a9", // The ID of this integration.
+  integrationID: "598d85d7-919d-4dac-a4a7-e98dbc6c30c6", // The ID of this integration.
   region: "eu-gb", // The region your integration is hosted in.
   serviceInstanceID: "3385ca4c-1b72-49c4-8929-de35d8c9d8c3", // The ID of your service instance.
-  onLoad: function(instance) { instance.render(); }
+  onLoad: async (instance) => {
+    await instance.render();
+  },
 };
-setTimeout(function(){
-  const t=document.createElement('script');
-  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+setTimeout(function () {
+  const t = document.createElement("script");
+  t.src =
+    "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
+    (window.watsonAssistantChatOptions.clientVersion || "latest") +
+    "/WatsonAssistantChatEntry.js";
   document.head.appendChild(t);
 });
